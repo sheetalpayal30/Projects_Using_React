bb49636d53d14df943f008c11753fd62d1256001
sheetalpayal30 <sheetalpayal3001@gmail.com>
2024-06-27 21:56:45 +0530

    4th Project.

---
+++ 04bg/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+function App() {
+  const [color,setColor]=useState("olive")
+
+  return (
+    
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor:color}}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2"></div>
+      </div>
+    
+    
+  )
+}
+
+export default App
